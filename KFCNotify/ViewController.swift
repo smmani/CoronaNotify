956cf6265ee79a131219bde8ee8b21e83fe36a39
Selvakumar <smurugan@blackboard.com>
2021-04-10 13:57:16 +0530

    Initial Commit

---
+++ KFCNotify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KFCNotify
+//
+//  Created by Selvakumar Murugan on 10/04/21.
+//  Copyright © 2021 Mani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
